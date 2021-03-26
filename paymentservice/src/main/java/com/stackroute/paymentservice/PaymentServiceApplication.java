c7f436824c1ccb687f06180783cdbf05b03b2bf1
AMARNATH.S <AMARNATH.S@ibm.com>
2021-03-26 16:31:36 +0530

    jwt implementation

--- paymentservice/src/main/java/com/stackroute/paymentservice/PaymentServiceApplication.java
+++ paymentservice/src/main/java/com/stackroute/paymentservice/PaymentServiceApplication.java
@@ -3,8 +3,13 @@ package com.stackroute.paymentservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.stackroute.paymentservice.jwtfilter.JwtFilter;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.XADataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import javax.servlet.Filter;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class, XADataSourceAutoConfiguration.class})
@@ -12,6 +17,15 @@ public class PaymentServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PaymentServiceApplication.class, args);
+    }
+
+    @Bean
+    public FilterRegistrationBean<?> jwtFilter() {
+
+        FilterRegistrationBean<Filter> filter = new FilterRegistrationBean<>();
+        filter.addUrlPatterns("/api/v1/*");
+        filter.setFilter(new JwtFilter());
+        return filter;
     }
 
 }
---
+++ paymentservice/src/test/java/com/stackroute/paymentservice/repository/PaymentRepositoryTest.java
@@ -0,0 +1,43 @@
+package com.stackroute.paymentservice.repository;
+
+
+import com.stackroute.paymentservice.model.PaymentServiceModel;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+
+@DataMongoTest
+public class PaymentRepositoryTest {
+
+    @Autowired
+    private PaymentServiceRepository paymentRepository;
+    private PaymentServiceModel payment;
+
+    @BeforeAll
+    public void setUp() throws Exception {
+
+        payment = new PaymentServiceModel();
+        payment.setCardNo("123456789");
+        payment.setCvv("789");
+        payment.setExpDate("08/21");
+
+    }
+    @Test
+    public void ceatePaymentTest() {
+        paymentRepository.insert(payment);
+        PaymentServiceModel actualPayment = paymentRepository.findById("123456789").get();
+        assertEquals(actualPayment.getCardNo(),payment.getCardNo());
+    }
+    @AfterAll
+    public void tearDown() throws Exception {
+
+        paymentRepository.deleteAll();
+    }
+
+
+
+}
